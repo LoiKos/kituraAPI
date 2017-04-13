b28a9db1606b4deeea28fb453efa7ef510a172e5
Loic LE PENN <loic.lepenn@orange.com>
2017-04-13 16:10:21 +0200

    Add Routing for Product

---
+++ Sources/ProductRouter.swift
@@ -0,0 +1,116 @@
+//
+//  File.swift
+//  kituraMbao
+//
+//  Created by Loic LE PENN on 13/04/2017.
+//
+//
+
+import Foundation
+import Kitura
+import SwiftKuery
+import LoggerAPI
+import SwiftyJSON
+
+
+
+public class ProductRouter {
+    
+    let service : ProductService
+    
+    public let router: Router
+    
+    init(database: Database){
+        service = ProductService(database: database)
+        router = Router()
+        setupRoutes()
+    }
+    
+    private func setupRoutes(){
+        router.get("/:id", handler : getProductById )
+        router.patch("/:id", handler : updateProductById )
+        router.delete("/:id", handler : deleteProductById )
+        router.get("/", handler : getProducts)
+        router.post("/", handler : createProduct )
+        router.error(handleError)
+    }
+    
+    
+    private func getProductById(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        if let id = request.parameters["id"] {
+            service.findById(id: id) { result, error in
+                response.status(.OK)
+                handleCompletion(result: result, error: error, response: response, next: next)
+            }
+        } else {
+            throw ErrorHandler.MissingRequireProperty("id")
+        }
+    }
+    
+    private func updateProductById(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        if let id = request.parameters["id"] {
+            guard let parsedBody = request.body else {
+                throw ErrorHandler.EmptyBody
+            }
+            
+            switch parsedBody {
+            case .json(let jsonBody):
+                try service.updateById(id: id, jsonBody: jsonBody) { result, error in
+                    response.status(.OK)
+                    handleCompletion(result:result, error: error,response: response,next: next)
+                }
+            default:
+                throw ErrorHandler.WrongType
+            }
+        } else {
+            throw ErrorHandler.MissingRequireProperty("id")
+        }
+    }
+    
+    private func deleteProductById(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        if let id = request.parameters["id"] {
+            service.deleteById(id: id) { result, error in
+                response.status(.OK)
+                handleCompletion(result: result, error: error, response: response, next: next)
+            }
+        } else {
+            throw ErrorHandler.MissingRequireProperty("id")
+        }
+    }
+    
+    private func getProducts(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        Log.info("Trying recover query paramers offset and limit")
+        
+        var limit:Int = 0
+        var offset:Int = 0
+        
+        if let paramOffset = request.queryParameters["offset"] {
+            offset = Int(paramOffset) ?? 0
+        }
+        if let paramLimit = request.queryParameters["limit"] {
+            limit = Int(paramLimit) ?? 0
+        }
+        
+        try service.findAll(limit: limit, offset: offset) { result, error in
+            response.status(.OK)
+            handleCompletion(result: result, error: error, response: response, next: next)
+        }
+    }
+    
+    private func createProduct(request: RouterRequest, response: RouterResponse, next: @escaping () -> Void) throws {
+        
+        guard let parsedBody = request.body else {
+            throw ErrorHandler.EmptyBody
+        }
+        
+        switch parsedBody {
+        case .json(let jsonBody):
+            try service.create(body: jsonBody) { result, error in
+                response.status(.created)
+                handleCompletion(result: result, error: error, response: response, next: next)
+            }
+        default:
+            throw ErrorHandler.WrongType
+        }
+    }
+}
---
+++ Sources/ProductService.swift
@@ -0,0 +1,250 @@
+//
+//  ProductService.swift
+//  kituraMbao
+//
+//  Created by Loic LE PENN on 13/04/2017.
+//
+//
+
+import Foundation
+import Kitura
+import LoggerAPI
+import SwiftKuery
+import SwiftyJSON
+
+class ProductService {
+    
+    let product = Product()
+    let db : Database
+    
+    init(database:Database){
+        self.db = database
+    }
+    
+    func create(body:JSON, oncompletion: @escaping ([String : Any?]?, Error?) -> ()) throws {
+        
+        guard let name = body["name"].string else {
+            throw ErrorHandler.MissingRequireProperty("name")
+        }
+        var tuples : [(Column,Any)] = [(Column,Any)]()
+        
+        tuples.append((product.name,name))
+        
+        if let picture = body["picture"].string {
+            tuples.append((product.picture, picture))
+        }
+        if let currency = body["currency"].string {
+            tuples.append((product.currency, currency))
+        }
+        if let vat = body["vat"].string {
+            tuples.append((product.vat, vat))
+        }
+        if let priceHT = body["priceHT"].string {
+            tuples.append((product.priceHT, priceHT))
+        }
+        
+        guard body.count == tuples.count else {
+            throw ErrorHandler.WrongParameter
+        }
+        
+        let id = generateRef()
+        tuples.append((product.refProduct, id))
+        tuples.append((product.creationDate, Date()))
+        
+        let query = Insert(into: product, valueTuples: tuples).suffix("RETURNING refProduct")
+        
+        db.executeQuery(query: query) { result in
+            switch result {
+            case .error(let error):
+                oncompletion(nil,error)
+            case .resultSet(let resultSet):
+                do {
+                    try oncompletion(resultSet.uniqueSingleRow(),nil)
+                } catch {
+                    Log.error("Malformed resulset")
+                    oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+                }
+            default:
+                oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+            }
+        }
+    }
+    
+    
+    func findAll(limit:Int = 0, offset:Int = 0, oncompletion: @escaping ([String : Any?]?, Error?) -> ()) throws {
+        
+        let query : Select = Select(from: product).order(by: .ASC(product.name))
+        var rawQuery : String = ""
+        
+        if limit > 0 {
+            rawQuery = try db.connection.descriptionOf(query: query.limit(to: limit))
+        } else if limit < 0 {
+            throw ErrorHandler.WrongParameter
+        } else {
+            rawQuery = try db.connection.descriptionOf(query: query)
+        }
+        
+        if offset > 0 {
+            rawQuery += " OFFSET \(offset)"
+        } else if offset < 0 {
+            throw ErrorHandler.WrongParameter
+        }
+        
+        count(){ countResult, errorCount in
+            guard let numberElement = countResult?["count"] else {
+                if let error = errorCount {
+                    oncompletion(nil, error)
+                } else {
+                    oncompletion(nil, ErrorHandler.DatabaseError("Impossible to retrieve database response"))
+                }
+                return
+            }
+            
+            self.db.executeQuery(query: rawQuery){ result in
+                switch result {
+                case .error(let error):
+                    oncompletion( nil, error )
+                case .successNoData:
+                    var dict : [String: Any?] = [String: Any?]()
+                    dict["total"] = numberElement
+                    if limit != 0 {
+                        dict["limit"] = limit
+                    }
+                    if offset != 0 {
+                        dict["offset"] = offset
+                    }
+                    dict["data"] = [[String: Any?]]()
+                    oncompletion(dict, nil)
+                case .resultSet(let resultSet):
+                    var dict : [String: Any?] = [String: Any?]()
+                    
+                    dict["total"] = numberElement
+                    if limit != 0 {
+                        dict["limit"] = limit
+                    }
+                    if offset != 0 {
+                        dict["offset"] = offset
+                    }
+                    dict["data"] = resultSet.asDictionaries()
+                    oncompletion(dict,nil)
+                default :
+                    oncompletion(nil, ErrorHandler.UnknowError)
+                }
+                return
+            }
+        }
+    }
+    
+    
+    
+    func findById(id:String, oncompletion: @escaping ([String:Any?]?, Error?) -> ()){
+        let select = Select(from:product).where(product.refProduct == id)
+        
+        db.executeQuery(query: select){ result in
+            switch result {
+            case .error(let error):
+                oncompletion(nil,error)
+            case .resultSet(let resultSet):
+                do {
+                    try oncompletion(resultSet.uniqueSingleRow(),nil)
+                } catch {
+                    Log.error("Malformed resulset")
+                    oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+                }
+            case .successNoData:
+                oncompletion(nil, ErrorHandler.NothingFoundFor("id : \(id)"))
+            case .success:
+                oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+            }
+        }
+    }
+    
+    func updateById(id: String, jsonBody: JSON, oncompletion: @escaping ([String:Any?]?,Error?) -> ()) throws {
+      /*  var updatedValue : [(Column,Any)] = [(Column,Any)]()
+        
+        if let name = jsonBody["name"].string {
+            updatedValue.append((product.name,name))
+        }
+        if let picture = jsonBody["picture"].string {
+            updatedValue.append((product.picture,picture))
+        }
+        
+        if let vat = jsonBody["vat"].float {
+            updatedValue.append((product.vat,vat))
+        }
+        if let merchantKey = jsonBody["merchantKey"].string {
+            updatedValue.append((product.merchantKey,merchantKey))
+        }
+        
+        if !updatedValue.isEmpty {
+            if jsonBody.count == updatedValue.count {
+                let query : Update = Update(product, set: updatedValue).where(product.refproduct == id).suffix("RETURNING *")
+                
+                db.executeQuery(query: query){ result in
+                    
+                    switch result {
+                    case .error(let error):
+                        oncompletion(nil,error)
+                    case .resultSet(let resultSet):
+                        do {
+                            try oncompletion(resultSet.uniqueSingleRow(),nil)
+                        } catch {
+                            Log.error("Malformed resulset")
+                            oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+                        }
+                    default:
+                        oncompletion(nil,ErrorHandler.UnexpectedDataStructure)
+                    }
+                }
+            } else {
+                throw ErrorHandler.WrongParameter
+            }
+        } else {
+            throw ErrorHandler.NothingToUpdate
+        }*/
+    }
+    
+    func deleteById(id:String, oncompletion: @escaping ([String:Any?]?,Error?) -> ()){
+      /*
+        let delete = Delete(from: product).where(product.refproduct == id).suffix("RETURNING *")
+        
+        db.executeQuery(query: delete){ queryResult in
+            switch(queryResult){
+            case .error(let error):
+                oncompletion(nil,error)
+            case .resultSet(let resultSet):
+                do {
+                    try oncompletion(resultSet.uniqueSingleRow(),nil)
+                } catch {
+                    Log.error("Malformed resulset")
+                    oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+                }
+            case .success(let result):
+                oncompletion(nil,ErrorHandler.UnexpectedDataStructure)
+            case .successNoData:
+                oncompletion(nil,ErrorHandler.NothingFoundFor("id : \(id)"))
+            }
+        } */
+    }
+    
+    private func count(oncompletion: @escaping ([String:Any?]?,Error?) -> ()){
+        let query : String = "select count(*) from products"
+        db.executeQuery(query:query) { queryResult in
+            switch(queryResult){
+            case .error(let error):
+                oncompletion(nil,error)
+            case .resultSet(let resultSet):
+                do {
+                    try oncompletion(resultSet.uniqueSingleRow(),nil)
+                } catch {
+                    Log.error("Malformed resulset")
+                    oncompletion(nil, ErrorHandler.UnexpectedDataStructure)
+                }
+            case .success:
+                oncompletion(nil,ErrorHandler.UnexpectedDataStructure)
+            case .successNoData:
+                oncompletion(nil,ErrorHandler.NothingFoundFor("in table \(self.product.tableName)"))
+            }
+        }
+    }
+}
