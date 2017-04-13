a1c02c950a1c20ab9edaeedec4f594caa4f6e20a
Loic LE PENN <loic.lepenn@orange.com>
2017-04-13 16:03:11 +0200

    update Swift Kuery PostgreSQL

--- Package.swift
+++ Package.swift
@@ -7,6 +7,6 @@ let package = Package(
     dependencies: [
         .Package(url: "https://github.com/IBM-Swift/Kitura.git", majorVersion: 1,minor:6),
         .Package(url: "https://github.com/IBM-Swift/HeliumLogger.git", majorVersion: 1,minor:6),
-        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git",majorVersion:0,minor:9)
+        .Package(url: "https://github.com/IBM-Swift/Swift-Kuery-PostgreSQL.git",majorVersion:0,minor:10)
     ]
 )
