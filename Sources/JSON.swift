ac0bfa10e8865d198d4a4234f42f2539910d7a76
LoiKos <loikos@mbp-de-loic.home>
2017-06-26 09:44:23 +0200

    Add a custom JSON serializer for Linux like JSONSerialization not available on Linux plateforms

---
+++ Sources/JSON.swift
@@ -0,0 +1,113 @@
+//
+//  JSON.swift
+//  kituraMbao
+//
+//  Created by Loic LE PENN on 21/06/2017.
+//
+//
+import SwiftyJSON
+import Foundation
+
+protocol JSONSerializable {
+    func toJSON() throws -> String
+}
+
+extension String: JSONSerializable {
+
+    func toJSON() -> String {
+        return "\"\(self)\""
+    }
+}
+
+extension Int : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Date : JSONSerializable {
+    func toJSON() -> String {
+        return String(describing: self).toJSON()
+    }
+}
+
+extension Int8 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Int16 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Int32 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Int64 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+// the same as Float
+extension Float32 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+// the same as Double
+extension Float64 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Float80 : JSONSerializable {
+    func toJSON() -> String {
+        return "\(self)"
+    }
+}
+
+extension Array : JSONSerializable {
+    func toJSON() throws -> String {
+        var out = [String]()
+        for item in self {
+            if let item = item as? JSONSerializable {
+               let serializeItem = try item.toJSON()
+               out.append(serializeItem)
+            } else {
+                throw JSONSerializableError.typeUnSerializable
+            }
+        }
+        return "[\(out.joined(separator: ", "))]"
+    }
+}
+
+extension Dictionary : JSONSerializable {
+    func toJSON() throws -> String {
+        var out = [String:JSONSerializable]()
+        for k in self.keys {
+            if let pv = self[k], let v = pv as? JSONSerializable {
+                let k = String(describing: k)
+                out[k] = try v.toJSON()
+            } else {
+                throw JSONSerializableError.typeUnSerializable
+            }
+        }
+        let array = out.map({ (key,value) -> String in  return "\(key.toJSON()):\(value)"})
+        return "{ \(array.joined(separator: ", ")) }"
+    }
+}
+
+enum JSONSerializableError : Error {
+    case typeUnSerializable
+    case notConvertible
+}
+
